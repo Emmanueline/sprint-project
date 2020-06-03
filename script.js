8aa08c5105513fcd379bd6632d08aaa53ec07ace
Emmanueline <47985021+Emmanueline@users.noreply.github.com>
2020-06-03 17:16:27 +0200

    js fonctionnel

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 let url = "https://ghibliapi.herokuapp.com/films";
 
-    fetch(url)
-    .then(response=>response.json())
-    .then((response => {
-        console.log(response);
+    
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+
+    let section = document.getElementById('section');
+    let inputButton = document.getElementById('bouton');
+    let title = document.getElementById('title');
+    let release_date = document.getElementById('release_date');
+
+    console.log(inputButton)
+    inputButton.addEventListener('click', function() {fetch(url)
+        .then(response=>response.json()) 
+        .then((response => { let random = getRandomInt(21)
+            console.log(response[random]);
+            title.innerText = response[random].title
+            release_date.innerText = response[random].release_date
+            section.innerText = response[random].description
+    
+        }))
+        .catch(error => alert('Erreur: '+ error))})
 
 
-    }))
-    .catch(error => alert('Erreur: '+ error))
